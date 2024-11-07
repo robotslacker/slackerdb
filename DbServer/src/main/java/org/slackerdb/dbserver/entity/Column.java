5bb1eba3524b8737900d26665d689016e33e58b0
robotslacker <184902652@qq.com>
2024-11-07 21:55:03 +0800

    move temporary. gitee bug!

--- DbServer/src/main/java/org/slackerdb/dbserver/entity/Column.java
+++
@@ -1,6 +0,0 @@
-package org.slackerdb.dbserver.entity;
-
-public class Column {
-    public int     columnLength;
-    public byte[]  columnValue;
-}
--- DbServer/src/main/java/org/slackerdb/dbserver/plsql/generate.bat
+++
@@ -1,8 +0,0 @@
-del *.interp
-del *.tokens
-del PlSqlParserBaseVisitor.java
-del PlSqlParserLexer.java
-del PlSqlParserParser.java
-del PlSqlParserVisitor.java
-java -jar antlr-4.13.2-complete.jar -package org.slackerdb.dbserver.plsql -visitor -Dlanguage=Java -no-listener PlSqlParser.g4
-
