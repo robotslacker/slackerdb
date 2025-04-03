3a3690110fab9e91900e766c0a79b3b5ea3a3ebb
robotslacker <184902652@qq.com>
2025-04-03 09:02:49 +0800

    0.1.3 release

--- connector-mysql/src/main/java/org/slackerdb/connector/mysql/exception/ConnectorMysqlBinlogNotExist.java
+++
@@ -1,9 +0,0 @@
-package org.slackerdb.connector.mysql.exception;
-
-import org.slackerdb.common.exceptions.ServerException;
-
-public class ConnectorMysqlBinlogNotExist extends ServerException {
-    public ConnectorMysqlBinlogNotExist(String errorMessage) {
-        super(errorMessage);
-    }
-}
